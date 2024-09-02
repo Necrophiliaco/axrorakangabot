8e1247b6e1de1a8b04fc1f0e5ca0462ad22bd173
Necrophiliaco <axrorbeksultan@gmail.com>
2024-09-02 13:41:45 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$UPSTREAM_REPO" ]; then
   echo "Cloning main Repository"
-  git clone https://github.com/Necrophiliaco/axrorakangabot /axrorakangabot
+  git clone https://github.com/Necrophiliaco/axrorakangabot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO"
   git clone $UPSTREAM_REPO /axrorakangabot
